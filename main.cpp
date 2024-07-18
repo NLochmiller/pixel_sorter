12ca7805d871e6ff71a49ec487ee02cb377c2042
Nathan Lochmiller <nlochmiller@outlook.com>
2024-07-17 17:53:18 -0700

    Very basic window is up and running

--- main.cpp
+++ main.cpp
@@ -14,7 +14,125 @@
 #error DearImGUI backend requires SDL 2.0.17+ because of SDL_RenderGeometry()
 #endif
 
-// Main code
+// Forward declerations
+void render(SDL_Renderer* renderer);
+int main_window(const ImGuiViewport* viewport);
+
+
 int main(int, char **) {
-  printf("Test print\n");
+  // Setup SDL
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER)
+      != 0) {
+    fprintf(stderr, "Error: %s\n", SDL_GetError());
+    return -1;
+  }
+
+  // From 2.0.18: Enable native IME.
+#ifdef SDL_HINT_IME_SHOW_UI
+  SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+#endif
+
+  // Create window with SDL_Renderer graphics context
+  SDL_WindowFlags window_flags =
+      (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+  SDL_Window *window = SDL_CreateWindow(
+      "Pixel Sorter", SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+  if (window == nullptr) {
+    fprintf(stderr, "Error: SDL_CreateWindow(): %s\n", SDL_GetError());
+    return -1;
+  }
+  SDL_Renderer *renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+  if (renderer == nullptr) {
+    SDL_Log("Error creating SDL_Renderer!");
+    return 0;
+  }
+
+    // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  (void)io;
+  // Enable Keyboard & gamepad controls
+  io.ConfigFlags |=
+      ImGuiConfigFlags_NavEnableKeyboard | ImGuiConfigFlags_NavEnableGamepad;
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+
+  // Setup SDL2 renderer backend
+  ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+  ImGui_ImplSDLRenderer2_Init(renderer);
+
+  bool done = false;
+  while (!done) {
+    // Poll and handle events (inputs, window resize, etc.)
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      ImGui_ImplSDL2_ProcessEvent(&event);
+      if (event.type == SDL_QUIT)
+        done = true;
+      if (event.type == SDL_WINDOWEVENT &&
+          event.window.event == SDL_WINDOWEVENT_CLOSE &&
+          event.window.windowID == SDL_GetWindowID(window))
+        done = true;
+    }
+
+    // Start the Dear ImGui frame
+    ImGui_ImplSDLRenderer2_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+
+    const ImGuiViewport *viewport = ImGui::GetMainViewport();
+    main_window(viewport);
+
+    // Rendering
+    render(renderer);
+  }
+  /* === END OF MAIN LOOP =================================================== */
+  
+  // Cleanup
+  ImGui_ImplSDLRenderer2_Shutdown();
+  ImGui_ImplSDL2_Shutdown();
+  ImGui::DestroyContext();
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
+
+// Render the entire window
+void render(SDL_Renderer* renderer) {
+  ImGuiIO &io = ImGui::GetIO();
+  ImGui::Render();
+  SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x,
+                     io.DisplayFramebufferScale.y);
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderClear(renderer);
+  ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), renderer);
+  SDL_RenderPresent(renderer);
+}
+
+// The main window, aka the background window
+// Returns non zero on error
+int main_window(const ImGuiViewport* viewport) {
+    static ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoCollapse |
+      ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoResize |
+      ImGuiWindowFlags_NoTitleBar;
+
+    // Make fullscreen
+    ImGui::SetNextWindowPos(viewport->WorkPos);
+    ImGui::SetNextWindowSize(viewport->WorkSize);
+
+    if (ImGui::Begin("Main window", NULL, window_flags)) {
+      static bool check = false;
+      ImGui::Checkbox("Test checkbox", &check);
+    }
+    ImGui::End();
+
+    return 0;
+}
+
