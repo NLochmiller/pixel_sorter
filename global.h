f714a5f16677166cb6e652bfc38f5695e3e3ded0
Nathan Lochmiller <nlochmiller@outlook.com>
2024-07-24 17:02:41 -0700

    simple 2d to 1d macro

--- global.h
+++ global.h
@@ -3,6 +3,7 @@
 
 // The default pixel format to be used in this program
 #define DEFAULT_PIXEL_FORMAT SDL_PIXELFORMAT_ABGR8888
+#define TWOD_TO_1D(_x_, _y_, _w_) _x_ + _y_ * _w_
 
 
 #endif // GLOBAL_H_
