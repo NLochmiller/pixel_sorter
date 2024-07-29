28ff7dca338a79c5d19f3ddbf19e16ece9d04477
Nathan Lochmiller <nlochmiller@outlook.com>
2024-07-29 13:29:02 -0700

    Refactor helpers out to another file to reduce clutter in main

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@
 #include "imfilebrowser.h"
 
 // Local includes
+#include "ImGui_SDL2_helpers.hpp"
 #include "LineInterpolator.hpp"
 #include "PixelSorter.hpp"
 #include "global.h"
@@ -24,8 +25,6 @@
 #if !SDL_VERSION_ATLEAST(2, 0, 17)
 #error DearImGUI backend requires SDL 2.0.17+ because of SDL_RenderGeometry()
 #endif
-
-
 
 const uint32_t DEFAULT_PIXEL_FORMAT = SDL_PIXELFORMAT_ABGR8888;
 
@@ -49,33 +48,13 @@ bool sort_wrapper(SDL_Renderer *renderer, SDL_Surface *&input_surface,
 }
 
 // Forward declerations
-SDL_Texture *updateTexture(SDL_Renderer *renderer, SDL_Surface *surface,
-                           SDL_Texture *texture);
-SDL_Surface *SDL_ConvertSurfaceFormat_MemSafe(SDL_Surface *src,
-                                              const Uint32 fmt);
-void render(SDL_Renderer *renderer);
 int main_window(const ImGuiViewport *viewport, SDL_Renderer *renderer,
                 SDL_Surface *&input_surface, SDL_Texture *&input_texture,
                 SDL_Surface *&output_surface, SDL_Texture *&output_texture,
                 std::filesystem::path *output_path);
 
 void handleMainMenuBar(ImGui::FileBrowser &inputFileDialog,
-                       ImGui::FileBrowser &outputFileDialog) {
-  if (ImGui::BeginMainMenuBar()) {
-    if (ImGui::BeginMenu("File")) {
-      ImGui::SeparatorText("Image files");
-      // File dialogs to select files
-      if (ImGui::MenuItem("Open", "", false)) {
-        inputFileDialog.Open();
-      }
-      if (ImGui::MenuItem("Export as", "", false)) {
-        outputFileDialog.Open();
-      }
-      ImGui::EndMenu();
-    }
-  }
-  ImGui::EndMainMenuBar();
-}
+                       ImGui::FileBrowser &outputFileDialog);
 
 int main(int, char **) {
   // Setup SDL
@@ -247,120 +226,24 @@ int main(int, char **) {
   return 0;
 }
 
-// Render the entire window
-void render(SDL_Renderer *renderer) {
-  ImGuiIO &io = ImGui::GetIO();
-  ImGui::Render();
-  SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x,
-                     io.DisplayFramebufferScale.y);
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  SDL_RenderClear(renderer);
-  ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), renderer);
-  SDL_RenderPresent(renderer);
-}
-
-// Display the texture
-// A width or height of 0 indicates that the dimension of the texture should be
-// used
-bool displayTexture(SDL_Renderer *renderer, SDL_Texture *texture,
-                    uint width = 0, uint height = 0) {
-  // Get dimensions of the texture only if either width or height is zero
-  if (width == 0 || height == 0) {
-    int texture_width = 0;
-    int texture_height = 0;
-    SDL_QueryTexture(texture, NULL, NULL, &texture_width, &texture_height);
-    // Assign the dimesions as needed
-    if (width == 0) {
-      width = texture_width;
-    }
-    if (height == 0) {
-      height = texture_height;
+// Handle the main menu bar
+void handleMainMenuBar(ImGui::FileBrowser &inputFileDialog,
+                       ImGui::FileBrowser &outputFileDialog) {
+  if (ImGui::BeginMainMenuBar()) {
+    if (ImGui::BeginMenu("File")) {
+      ImGui::SeparatorText("Image files");
+      // File dialogs to select files
+      if (ImGui::MenuItem("Open", "", false)) {
+        inputFileDialog.Open();
+      }
+      if (ImGui::MenuItem("Export as", "", false)) {
+        outputFileDialog.Open();
+      }
+      ImGui::EndMenu();
     }
   }
-
-  ImGui::Image((void *)texture, ImVec2(width, height));
-  return true;
+  ImGui::EndMainMenuBar();
 }
-
-// For width and height, 0 indicates to use the respective dimension of the
-// surface
-bool displaySurface(SDL_Renderer *renderer, SDL_Surface *surface,
-                    uint width = 0, uint height = 0) {
-  if (surface == NULL) {
-    return false;
-  }
-
-  SDL_Texture *texture_ptr = SDL_CreateTextureFromSurface(renderer, surface);
-  if (texture_ptr == NULL) {
-    printf("Bad texture pointer");
-    return false;
-  }
-
-  // Adjust width and height to the images if desired (width or height = 0)
-  if (width == 0) {
-    width = surface->w;
-  }
-  if (height == 0) {
-    height = surface->h;
-  }
-
-  ImGui::Image((void *)texture_ptr, ImVec2(width, height));
-  return true;
-}
-
-
-// Update the texture to whatever surface is.
-// texture will always be destoryed, and can be NULL
-// if surface is NULL, texture is returned
-SDL_Texture *updateTexture(SDL_Renderer *renderer, SDL_Surface *surface,
-                           SDL_Texture *texture) {
-  if (surface == NULL) {
-    return NULL;
-  }
-
-  if (texture != NULL) {
-    // Memory cleanup
-    SDL_DestroyTexture(texture);
-    texture = NULL;
-  }
-
-  // Create the new texture to use
-  texture = SDL_CreateTextureFromSurface(renderer, surface);
-  if (texture == NULL) {
-    fprintf(stderr, "updateTexture: Could not create texture from surface\n");
-  }
-
-  return texture;
-}
-
-
-/*
- * A custom version of SDL_ConvertSurfaceFormat with the following differences:
- *  1) Does not require the unused flag parameter
- *  2) Always does the following
- *    - Creates a copy of surface in pixel_format
- *    - Frees old surface which is in the original format we don't want
- *  3) The input surface should be immediatly replaced with the returned surface
- *
- * Inputs:
- *   src - the existing SDL_Surface structure to convert.
- *   fmt - the SDL_PixelFormat structure that the new surface is optimized for.
- *
- * Output:
- *   A pointer to a copy of src in fmt. Never returns src.
- *   Can return NULL if src is NULL or another error occurs
- */
-SDL_Surface *SDL_ConvertSurfaceFormat_MemSafe(SDL_Surface *src,
-                                              const Uint32 fmt) {
-  if (src == NULL) {
-    return NULL;
-  }
-  SDL_Surface *new_surface = SDL_ConvertSurfaceFormat(src, fmt, 0);
-  SDL_FreeSurface(src);
-  src = NULL;
-  return new_surface;
-}
-
 
 // The main window, aka the background window
 // Returns non zero on error
@@ -425,7 +308,8 @@ int main_window(const ImGuiViewport *viewport, SDL_Renderer *renderer,
 
       // Display output image zoomed in to percent
       if (output_texture != NULL) {
-        displayTexture(renderer, output_texture, output_surface->w * zoom_percent,
+        displayTexture(renderer, output_texture,
+                       output_surface->w * zoom_percent,
                        output_surface->h * zoom_percent);
       }
     }
