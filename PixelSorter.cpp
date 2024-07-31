1af15a362c61529e1cefbbb4df392a7137fed69b
Nathan Lochmiller <nlochmiller@outlook.com>
2024-07-31 13:31:11 -0700

    Working for 0-135

--- PixelSorter.cpp
+++ PixelSorter.cpp
@@ -9,61 +9,70 @@ void PixelSorter::sort(PixelSorter_Pixel_t *&input_pixels,
                        int numPoints, int width, int height, int startX,
                        int startY, int endX, int endY,
                        SDL_Surface *input_test) {
+  int deltaX = endX - startX;
+  int deltaY = endY - startY;
+
+  // Forward decleration
+  int x = 0;
+  int y = 0;
 
   /*
    * Now in the perspective of 1 dimension L where L is the dimension with
-   * greater change, that is:
-   *   if |deltaX| >= |deltaY|, L = X.
-   *   if |deltaX| <  |deltaY|, L - Y.
+   * less change, that is:
+   *   if |deltaX| <= |deltaY|, L = X.
+   *   if |deltaX| >  |deltaY|, L = Y.
    *
    * There is also another dimension S where S is the opposite of L, that is:
    *   if L = X, S = Y.
    *   If L = Y, S = X
    */
-
-  int deltaX = endX - startX;
-  int deltaY = endY - startY;
-
+  int minL = 0; // The minimum value of L to sort with
+  int maxL = 0; // The maximum value of L to sort with
   // Use pointers to save on lines of code
-  int *minL = NULL;   // The minimum value of L to sort with
-  int *maxL = NULL;   // The maximum value of L to sort with
   int *deltaL = NULL; // The max of deltaX deltaY
   int *deltaS = NULL; // The min of deltaX deltaY
-  // The two dimension indexes
-  // int l = 0, s = 0;
-  int *l = NULL, *s = NULL;
-
-  // Change deltaL by deltaS where S is the dimension with a smaller delta
-  // without changing where the center of the N lines are
-  if (std::abs(deltaX) <= std::abs(deltaY)) { // X changes more than Y or same
-    // Go from startX to endX or endX to startX, whichever way means going from
-    // min to max
-    minL = (startX <= endX) ? &startX : &endX;
-    maxL = (startX <= endX) ? &endX : &startX;
+  // The two dimension indexes, for L and S respectively
+  int *l = NULL;
+  int *s = NULL;
+  // // Change deltaL by deltaS where S is the dimension with a smaller delta
+  // // without changing where the center of the N lines are
+  if (std::abs(deltaX) <= std::abs(deltaY)) { // X changes less or same as Y
+    l = &x;
+    maxL = width;
     deltaL = &deltaX;
     deltaS = &deltaY;
-    s = &startY;
-  } else { // Y changes more than X
-    minL = (startY <= endY) ? &startY : &endY;
-    maxL = (startY <= endY) ? &endY : &startY;
+    // s = &y;
+    // if (deltaY >= 0) {
+    //   y = startY;
+    // } else {
+    //   y = endY;
+    // }
+  } else { // Y changes less than X
+    l = &y;
+    maxL = height;
     deltaL = &deltaY;
     deltaS = &deltaX;
-    s = &startX;
+    // s = &x;
+    // if (deltaX >= 0) {
+    //   x = startX;
+    // } else {
+    //   x = endX;
+    // }
   }
-  l = minL;
-  
+
   /* TODO: Disable after getting sweeping across L to work
   // Extend start and end by offset, causing the deltaL to grow by 2*deltaS
-  int offset = ((*deltaL >= 0) ? *deltaS : -*deltaS);
-  (*startL) += offset;
-  (*endL) -= offset;
+  int offset = std::abs(*deltaS);
+  (*minL) -= offset;
+  (*maxL) += offset;
   */
 
-  printf("start(%d %d) | end(%d %d) | l m%d M%d D%d | s%d D%d\n", startX,
-         startY, endX, endY, *minL, *maxL, *deltaL, *s, *deltaS);
+  printf("start(%d %d) | end(%d %d) | l m%d M%d D%d | s D%d\n", startX, startY,
+         endX, endY, minL, maxL, *deltaL, *deltaS);
   // For each line along l, increase it by 1
-  for (l = minL; *l < *maxL; (*l)++) {
-    printf("  start(%d %d) end(%d %d)\n", startX, startY, endX, endY);
+  for (; *l < maxL; (*l)++) {
+    printf("%d %d\n", x, y);
+
     /*
      * For each line:
      *  while out of bounds: move along line
@@ -83,12 +92,14 @@ void PixelSorter::sort(PixelSorter_Pixel_t *&input_pixels,
     // Test code to check each point in line by drawing it to the screen
 
     for (int i = 0; i < numPoints; i++) {
-      int x = points[i].first + startX;
-      int y = points[i].second + startY;
 
-      if (0 <= x && x < width && 0 <= y && y < height) { // if in bounds
-        output_pixels[TWOD_TO_1D(x, y, width)] = SDL_MapRGB(
-            input_test->format, 255 * (numPoints - i) / numPoints, 0, 0);
+      int px = points[i].first + x;
+      int py = points[i].second + y;
+      printf("(%d %d)\n", px, py);
+      if (0 <= px && px < width && 0 <= py && py < height) { // if in bounds
+        output_pixels[TWOD_TO_1D(px, py, width)] =
+            SDL_MapRGB(input_test->format, 255 * (numPoints - i) / numPoints,
+                       255 * (maxL - *l) / maxL, 0);
       }
     }
     /* TEST CODE END  ======================================================= */
